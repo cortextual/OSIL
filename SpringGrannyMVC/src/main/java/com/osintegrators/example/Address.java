f957a5cac3732bc2c36ed3511c35fe9729cea8ba
Andrew D. Ball <anball@gmail.com>
2012-10-27 17:56:14 -0400

    Add Hibernate Search annotations for the Address entity.

--- SpringGrannyMVC/src/main/java/com/osintegrators/example/Address.java
+++ SpringGrannyMVC/src/main/java/com/osintegrators/example/Address.java
@@ -1,5 +1,10 @@
 package com.osintegrators.example;
 
+import org.apache.solr.analysis.LowerCaseFilterFactory;
+import org.apache.solr.analysis.SnowballPorterFilterFactory;
+import org.apache.solr.analysis.StandardTokenizerFactory;
+import org.hibernate.search.annotations.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,16 +15,28 @@ import javax.persistence.NamedQuery;
 @Entity
 @NamedQueries({@NamedQuery(name="Address.findAll",query="select a from Address a"),
 	           @NamedQuery(name="Address.findByName",query="select a from Address a where a.name = ?1")})
+@Indexed
+@AnalyzerDef(name = "customanalyzer",
+    tokenizer = @TokenizerDef(factory = StandardTokenizerFactory.class),
+    filters = {
+        @TokenFilterDef(factory = LowerCaseFilterFactory.class),
+        @TokenFilterDef(factory = SnowballPorterFilterFactory.class, params = {
+            @Parameter(name = "language", value = "English")
+        })
+    })
 public class Address {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 
-
+    @Field(index=Index.TOKENIZED, store=Store.NO)
 	private String name;
+    @Field(index=Index.TOKENIZED, store=Store.NO)
 	private String email;
+    @Field(index=Index.TOKENIZED, store=Store.NO)
 	private String phone;
+    @Field(index=Index.TOKENIZED, store=Store.NO)
 	private String address;
 	
 	public Address() {}
